51f5833c506344954dc059d2adffbab06178f71f
Ron Gruesbeck <rongruesbeck@gmail.com>
2016-03-26 12:59:16 -0700

    add clean task and rename build to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('default', function() {
-    console.log('hello gulp!');
+
+var paths = {
+    index: 'src/index.html',
+    js: 'src/js/**/*.js',
+    lib: 'src/lib/**/*.js',
+    images: 'src/images/**/*',
+    css: 'src/css/**/*.css'
+};
+
+gulp.task('clean', function(cb) {
+    del(['dist'], cb);
+});
+
+
+gulp.task('default',
+    [
+	'clean'
+    ],
+    function() {
 });
